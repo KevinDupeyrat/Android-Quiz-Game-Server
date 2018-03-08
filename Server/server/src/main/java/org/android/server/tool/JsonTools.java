2ca4e32a1b3d116c73bdfb89728e487baca93230
KevinDupeyrat <k.dupeyrat@protonmail.com>
2018-03-08 10:12:16 +0100

    Externalisation des données

---
+++ Server/server/src/main/java/org/android/server/tool/JsonTools.java
@@ -0,0 +1,73 @@
+package org.android.server.tool;
+
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.util.List;
+
+import com.google.gson.reflect.TypeToken;
+import org.android.server.bean.Amis;
+import org.android.server.bean.Question;
+
+
+/**
+ * Created by dupeyrat on 26/02/18.
+ *
+ * Class Outil pour la gestion des fichier Json
+ * Elle permetra de générer à partir de fichier Json
+ * des Objets
+ */
+
+public class JsonTools {
+
+
+    private static final String projectPath = "src/main/resources/static/";
+
+    /**
+     * Méthode qui permet de récupérer les question/reponses
+     * d'un fichier JSON vers un objet Question
+     * Utilisation de l'API GSON de Google
+     * @return
+     */
+    public static List<Question> getQuestion() throws IOException {
+
+        File jsonFile = new File(projectPath + "question.json");
+        byte[] bytes = Files.readAllBytes(jsonFile.toPath());
+        String jsonString = new String(bytes, "UTF-8");
+
+        System.out.println(jsonString);
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Question>>(){}.getType();
+        List<Question> questionList = gson.fromJson(jsonString, type);
+
+        return questionList;
+    }
+
+
+    /**
+     * Méthode qui permet de convertir un fichier Json
+     * passé en paramètre en une liste
+     * de Question qui est renvoyé
+     * @return
+     */
+    public static List<Amis> getAmis() throws IOException {
+
+        File jsonFile = new File(projectPath + "friends.json");
+        byte[] bytes = Files.readAllBytes(jsonFile.toPath());
+        String jsonString = new String(bytes, "UTF-8");
+
+        System.out.println(jsonString);
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Amis>>(){}.getType();
+        List<Amis> amisList = gson.fromJson(jsonString, type);
+
+
+        return amisList;
+    }
+
+}
---
+++ Server/server/src/main/java/org/android/server/web/ServerControler.java
@@ -0,0 +1,182 @@
+package org.android.server.web;
+
+import org.android.server.bean.*;
+import org.android.server.tool.JsonTools;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+
+/**
+ * Class contrôler qui permet d'effectuer des traitement en fonction de l'URL.
+ * Il s'agit d'un Web Service.
+ * Voici les etats pour que deux joueurs puissent jouer enssemble :
+ *
+ *  1 - Ce connecter au server : checkAttending()
+ *  2 - Récupérer la liste d'amis : getFriends()
+ *  3 - Envoyer une requête pour un amis : putRequestFriend()
+ *  4 - L'ami verifie périodiquement qu'il n'a pas une requête pour lui sur le server
+ *      à l'aide d'un Timer qui envoie des requêtes : checkRequest()
+ *  5 - L'ami renvoie sa reponse sur le serveur : responceRequestFriend()
+ *  6 - Si l'ami à dit 'oui' on lui envoie la question
+ *  5'- Nous vérifions periodiquement que l'amis a repondu à la requête à
+ *      l'aide d'un Timer en envoyant une requête: getResponseFriend()
+ *  6'- Si la requête est ok on m'envoie la question
+ *
+ *  7 - Les reponses sont envoyer sur le server et renvoie le score de chaque joueur
+ *  (Nous laisson au client la gestion de la reponse (bonne ou fausse)), puis une autre
+ *  question en renvoyer aux deux joueurs.
+ *
+ *  8 - Au bout de 5 Questions (sauf en cas dégalité dans ce cas on renvoie une question
+ *  jusqu'a trouver un gagnant) on déclare la fin du jeu
+ *  (Nous laisson au client la gestion de l'affichage du gagnant).
+ *  Nous supprimons la demande de requête.
+ *
+ */
+
+
+
+@Controller
+public class ServerControler {
+
+
+    private final String serverKey = "123456";
+    private Amis amisControler = new Amis();
+    private Question questionControler = new Question();
+    private FriendRequest friendRequestControler = new FriendRequest();
+    private Jeu jeuControler = new Jeu();
+
+
+
+
+    public ServerControler() {
+
+        // Récupération des amis du fichier JSON
+        try {
+            amisControler.addAllFriendListe(JsonTools.getAmis());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // Récupération des question du fichier JSON
+        try {
+            questionControler.addAllQuestionList(JsonTools.getQuestion());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    
+
+
+    @RequestMapping(value = "/checkAttending", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String checkAttending(@RequestParam("key") String key,
+    @RequestParam("id") String id) throws Exception{
+
+        return amisControler.checkAttending(key, id, serverKey);
+    }
+
+
+    @RequestMapping(value = "/disconnect", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String disconnect(@RequestParam("key") String key,
+                                 @RequestParam("id") String id) throws Exception{
+
+        return amisControler.disconnect(key, id, serverKey);
+    }
+
+
+    @RequestMapping(value = "/getFriends", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String getFriends(@RequestParam("key") String key) throws Exception{
+
+        return amisControler.getFriends(key, serverKey);
+    }
+
+
+    @RequestMapping(value = "/getQuestions", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String getQuestions(@RequestParam("key") String key) throws Exception{
+
+        return questionControler.getQuestion(key, serverKey);
+    }
+
+
+    @RequestMapping(value = "/putRequestFriend", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String putRequestFriend(@RequestParam("key") String key,
+    @RequestParam("my_id") String my_id, @RequestParam("friend_id") String friend_id) throws Exception{
+
+        return friendRequestControler.putRequestFriend(
+                key, serverKey, my_id, friend_id, amisControler.getAmisList());
+    }
+
+
+    @RequestMapping(value = "/checkRequest", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String checkRequest(@RequestParam("key") String key,
+                                   @RequestParam("my_id") String my_id) throws Exception{
+
+        return friendRequestControler.checkRequest(key, serverKey, my_id, amisControler.getAmisList());
+    }
+
+
+    @RequestMapping(value = "/responceRequestFriend", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String responceRequestFriend(@RequestParam("key") String key,
+                                     @RequestParam("my_id") String my_id,
+                                        @RequestParam("friend_id") String friend_id,
+                                        @RequestParam("response") String response) throws Exception{
+
+        return friendRequestControler.responceRequestFriend(key, serverKey, my_id, friend_id,
+                amisControler.getAmisList(), response);
+    }
+
+
+    @RequestMapping(value = "/getResponseFriend", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String getResponseFriend(@RequestParam("key") String key,
+                                     @RequestParam("my_id") String my_id) throws Exception{
+
+        return friendRequestControler.getResponseFriend(key, serverKey, my_id, amisControler.getAmisList(),
+                jeuControler.getJeuList());
+    }
+
+
+
+    /**
+     * Méthode qui permet de vérifier la reponse obtenu et
+     * de renvoyer une autre question
+     *
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/checkResponseQuestion", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String checkResponseQuestion(@RequestParam("key") String key) throws Exception   {
+
+
+
+        //TODO: traitement de la reponse
+
+        return getQuestions(key);
+    }
+
+
+}
